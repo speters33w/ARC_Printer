41ef20bc23ba326b4549cf1991e241af79b91762
speters33w <speters33w@gmail.com>
2025-07-15 11:21:39 -0400

    Tweaks to python file

--- capture_zpl.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ZPL Capture Tool
-echo ================
-
-python "%USERPROFILE%\Documents\ProblemSolve\zpl_capture.py"
-
-echo.
-echo ZPL data saved to output.zpl
-echo.
--- zpl_capture.py
+++ zpl_capture.py
@@ -1,13 +1,15 @@
 import socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('localhost', 5964))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        # with open('c:\OneDrive\Documents\ProblemSolve\output.zpl', 'wb') as f:
-        with open('output.zpl', 'wb') as f:
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind(('localhost', 5964))
+    server_socket.listen()
+    print("Waiting for ZPL data... Send a print job to localhost:5964")
+    client_connection, client_address = server_socket.accept()
+    with client_connection:
+        # with open('c:\OneDrive\Documents\ProblemSolve\output.zpl', 'wb') as output_file:
+        with open('output.zpl', 'wb') as output_file:
             while True:
-                data = conn.recv(1024)
-                if not data:
+                received_data = client_connection.recv(1024)
+                if not received_data:
                     break
-                f.write(data)
+                output_file.write(received_data)
