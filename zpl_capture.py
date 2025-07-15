e0f89f49bbc440afa447f0dd0d146a500abd697e
speters33w <speters33w@gmail.com>
2025-07-15 13:11:36 -0400

    Added graphic Peccy header

--- zpl_capture.py
+++ zpl_capture.py
@@ -2,8 +2,8 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind(('localhost', 5964))
-    server_socket.listen()
     print("Waiting for ZPL data... Send a print job to localhost:5964")
+    server_socket.listen()
     client_connection, client_address = server_socket.accept()
     with client_connection:
         # with open('c:\OneDrive\Documents\ProblemSolve\output.zpl', 'wb') as output_file:
